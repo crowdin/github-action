b71982d5435733ef36eabc95909e654f74d4c10f
Vitalii Smolinskyi <46049489+VBeytok@users.noreply.github.com>
2021-11-02 15:10:08 +0200

    Update entrypoint.sh
    minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -149,24 +149,19 @@ push_to_branch() {
     git checkout -b "${LOCALIZATION_BRANCH}"
   fi
 
-  if [ -n "$(git status -s)" ]; then
-    echo "PUSH TO BRANCH ${LOCALIZATION_BRANCH}"
+  git add .
 
-    git add .
-
-    if [ ! -n "$(git status -s)" ]; then
-      echo "NOTHING TO COMMIT"
-      return
-    fi
-
-    git commit --no-verify -m "${INPUT_COMMIT_MESSAGE}"
-    git push --no-verify --force "${REPO_URL}"
-
-    if [ "$INPUT_CREATE_PULL_REQUEST" = true ]; then
-      create_pull_request "${LOCALIZATION_BRANCH}"
-    fi
-  else
+  if [ ! -n "$(git status -s)" ]; then
     echo "NOTHING TO COMMIT"
+    return
+  fi
+
+  echo "PUSH TO BRANCH ${LOCALIZATION_BRANCH}"
+  git commit --no-verify -m "${INPUT_COMMIT_MESSAGE}"
+  git push --no-verify --force "${REPO_URL}"
+
+  if [ "$INPUT_CREATE_PULL_REQUEST" = true ]; then
+    create_pull_request "${LOCALIZATION_BRANCH}"
   fi
 }
 
